a20c79f22262b3da9b5fe89caec95b7b1c60e828
Palmeri <palmerisamuele.pro@gmail.com>
2024-08-23 00:46:40 +0200

    v2.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
-
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/path-to-cdn/', // Set this to your CDN base path
+  },
   module: {
     rules: [
+      // CSS handling
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      // Image and other asset handling
       {
         test: /\.(png|jp(e*)g|svg|gif)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][hash][ext][query]',
+        },
       },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
 };
